06cfe65dd33cf4a1855b1530b1ac88528e42c52e
dinasamer559@yahoo.com <dinasamer559@yahoo.com>
2020-08-19 01:48:49 +0200

    fix bug

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,52 +1,62 @@
-import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import {Observable, Subject} from 'rxjs';
 import {Question} from './question';
 import {Quiz} from './quiz';
 
 @Injectable()
 
-export  class ApiService {
+export class ApiService {
 
-  private  selectedQuestion = new Subject<Question>();
+  private selectedQuestion = new Subject<Question>();
   questionSelected = this.selectedQuestion.asObservable();
 
-  private  selectedQuiz = new Subject<Quiz>();
+  private selectedQuiz = new Subject<Quiz>();
   quizSelected = this.selectedQuiz.asObservable();
 
-  constructor(private  http: HttpClient) {}
-  getQuestions(quizId: number): Observable<Question[]>{
-     return this.http.get<Question[]>(`http://localhost:60197/api/questions/${quizId}`);
-    }
-    getQuizzes(): Observable<Quiz>{
+  constructor(private  http: HttpClient) {
+  }
+
+  getQuestions(quizId: number): Observable<Question[]> {
+    return this.http.get<Question[]>(`http://localhost:60197/api/questions/${quizId}`);
+  }
+
+  getQuizzes(): Observable<Quiz> {
     return this.http.get<Quiz>('http://localhost:60197/api/quizzes');
   }
-   getAllQuizzes(): Observable<Quiz[]> {
+
+  getAllQuizzes(): Observable<Quiz[]> {
     return this.http.get<Quiz[]>('http://localhost:60197/api/quizzes/all');
-    }
-    postQuestion(question: Question ): void {
-    this.http.post('http://localhost:60197/api/questions', question).subscribe( res => {
+  }
+
+  postQuestion(question: Question): void {
+    this.http.post('http://localhost:60197/api/questions', question).subscribe(res => {
       console.log(res);
     });
   }
-    putQuestion(question: Question): void{
-      this.http.put('http://localhost:60197/api/questions', + question.id).subscribe( res => {
-        console.log(res);
-      });
-    }
+
+  putQuestion(question: Question): void {
+    this.http.put('http://localhost:60197/api/questions', +question.id).subscribe(res => {
+      console.log(res);
+    });
+  }
+
   postQuiz(quiz: Quiz): void {
-    this.http.post('http://localhost:60197/api/quizzes', quiz).subscribe( res => {
+    this.http.post('http://localhost:60197/api/quizzes', quiz).subscribe(res => {
       console.log(res);
     });
   }
-  putQuiz(quiz: Quiz ): void{
-    this.http.put('http://localhost:60197/api/quizzes', + quiz.id).subscribe( res => {
+
+  putQuiz(quiz: Quiz): void {
+    this.http.put('http://localhost:60197/api/quizzes', +quiz.id).subscribe(res => {
       console.log(res);
     });
   }
-  selectQuestion(question: Question ): void {
+
+  selectQuestion(question: Question): void {
     this.selectedQuestion.next(question);
   }
+
   selectQuiz(quiz: Quiz): void {
     this.selectedQuiz.next(quiz);
   }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { Router} from '@angular/router';
+import {Credentials} from './credentials';
 
 @Injectable()
 export class AuthService {
   constructor(private  http: HttpClient, private  router: Router) {}
-  get isAuthenticated(): void{
+  get isAuthenticated(): boolean{
     return !!localStorage.getItem('token');
   }
-  register(credentials): void {
+  register(credentials: Credentials): void {
     this.http.post(`http://localhost:60197/api/account`, credentials, {responseType: 'text'}).subscribe(res => {
       this.authenticate(res);
   });
 }
-  login(credentials): void {
+  login(credentials: Credentials): void {
     this.http.post(`http://localhost:60197/api/account/login`, credentials, {responseType: 'text'}).subscribe(res => {
       this.authenticate(res);
     });
   }
-  authenticate(res): void{
+  authenticate(res: string): void{
     localStorage.setItem('token', res);
     this.router.navigate(['/']);
   }
